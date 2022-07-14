3bdcb65199a1926865ca95c00d68d6ab9c76dd30
yujial <yujia_liu_2@sfu.ca>
2022-07-13 20:32:44 -0700

    add ibmq and reference

--- Deutsch.py
+++ Deutsch.py
@@ -27,6 +27,22 @@ program.barrier()
 
 program.measure(qr[0],cr)
 
-job = qiskit.execute( program, qiskit.BasicAer.get_backend('qasm_simulator') )
+job1 = qiskit.execute( program, qiskit.BasicAer.get_backend('qasm_simulator') )
 
-print(job.result().get_counts())
+# ibmq
+qiskit.IBMQ.save_account('your token',overwrite=True) #Replace the text my_token for your own token
+qiskit.IBMQ.load_account() # load the token
+provider = qiskit.IBMQ.get_provider("ibm-q")
+backend = provider.get_backend('ibmq_lima') # select the name of the quatum computer to use
+job2 = qiskit.execute(program, backend)
+
+# show the result
+print(job1.result().get_counts())
+
+print(job2.result().get_counts())
+#Reference
+#https://leimao.github.io/blog/Deutsch-Algorithm/
+#Lecture Notes "Weeks 7-8 2022.pdf"
+#"program.barrier()" https://github.com/atilsamancioglu/QX07-DeutschAlgorithm
+#"use X Gate to simulate constant functions" https://github.com/MakotoNakai/Deutch-Algorithm/blob/master/Deutch_constant.py
+#"use IBMQ" https://fullstackquantumcomputation.tech/blog/post-tutorial-0-Hello-world/
